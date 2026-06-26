d699e20282439eb5b3e9196e93e569baba806321
root <root@pi-star>
2026-06-26 07:21:51 -0400

    incl HTPASSWD

--- pistar-bootconfig.sh
+++ pistar-bootconfig.sh
@@ -69,6 +69,7 @@ rm -f /tmp/config_restore/*.nmconnection 2>&1   # in case they didn't get moved
 mv -f /tmp/config_restore/ircddblocal.php /var/www/dashboard/config/ 2>&1
 mv -f /tmp/config_restore/config.php /var/www/dashboard/config/ 2>&1
 mv -f /tmp/config_restore/wpa_supplicant.conf /etc/wpa_supplicant/ 2>&1
+mv -f /tmp/config_restore/.htpasswd /var/www/ 2>&1
 mv -f /tmp/config_restore/* /etc/ 2>&1
 
 # Just in case
